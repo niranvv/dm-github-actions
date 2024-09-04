a78d83ffa3770d4ebea4e0de232d12e00783a4fd
Niran Vijayakumar <niranvv@gmail.com>
2024-09-04 21:18:49 +0530

    Hello actions first checkin

---
+++ src/web/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get(["/", "/index.html"], (req, res) => {
+    res.send("Hello World!");
+});
+
+if (process.env.NODE_ENV !== "test") {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+}
+
+module.exports = app;
